2363ec8160e33805b954487dc2f2d08998be0bc8
andresangarita10 <andres.tiikas@gmail.com>
2023-10-15 18:45:40 -0500

    feat: :tada:
    @AndresAngarita10

---
+++ Persistence/ApiContext.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+    }
+}
